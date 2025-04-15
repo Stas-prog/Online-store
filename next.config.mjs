bf4578266aeafc0e910df69bf3ffc4ef0cd8bf55
Stas-prog <skubav86@gmail.com>
2025-04-15 03:44:47 +0300

    next2 commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@ import { hostname } from 'os';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+env: {
+    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_WIX_CLIENT_ID: process.env.NEXT_PUBLIC_WIX_CLIENT_ID,
+    FEATURED_PRODUCTS_CATEGORY_ID: process.env.FEATURED_PRODUCTS_CATEGORY_ID,
+    NEXT_PUBLIC_WIX_APP_ID: process.env.NEXT_PUBLIC_WIX_APP_ID
+}
+
     images: {
         remotePatterns: [
             {
