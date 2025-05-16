cb1fe8c9d542fcbc340e6b1650ea3f6dd86a899c
Stas-prog <139768018+Stas-prog@users.noreply.github.com>
2025-05-17 02:15:56 +0300

    Update middleware.tsx

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -5,13 +5,11 @@ import {NextRequest, NextResponse} from "next/server"
 export const middleware = async (request: NextRequest) => {
     if(!request.cookies.get("session")) {
         const response = NextResponse.next()
-        const wixClient=createClient({
+        const wixClient = createClient({
             auth: OAuthStrategy({clientId:process.env.NEXT_PUBLIC_WIX_CLIENT_ID!})
         })
         response.cookies.set("session", JSON.stringify(await wixClient.auth.generateVisitorTokens()))
               
-        console.log(response)
-
         return response
     }
 }
